0428438a0cfd154f5e0385c153bd463582132f35
Cameron Chandler <webstormcam@gmail.com>
2022-04-01 05:06:41 -0400

    Advice produces in console on button click
    Now I gotta make it come out in html lol

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let url = 'https://api.adviceslip.com/advice';
+const button = document.getElementById('circle_button')
 
-fetch(url)
-.then(res => res.json())
-.then((out) => {
-  console.log( out.slip.id, out.slip.advice);
+
+button.addEventListener('click',function(){
+  let url = 'https://api.adviceslip.com/advice';
+
+  fetch(url)
+  .then(res => res.json())
+  .then((out) => {
+    console.log( out.slip.id, out.slip.advice);
+  })
+  .catch(err => { throw err });
 })
-.catch(err => { throw err });
+
