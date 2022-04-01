534f3da7b5b2e5885d3b5558008a3405e33a471f
Cameron Chandler <webstormcam@gmail.com>
2022-04-01 05:43:46 -0400

    made it spin once lol

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const button = document.getElementById('circle_button')
-
+let dice = document.getElementById('dice')
 
 button.addEventListener('click',function(){
   let url = 'https://api.adviceslip.com/advice';
-
+  dice.style.transform =`rotate(75deg)`
   fetch(url)
   .then(res => res.json())
   .then((out) => {
@@ -11,4 +11,17 @@ button.addEventListener('click',function(){
   })
   .catch(err => { throw err });
 })
+
+
+
+
+// function rotateImg(){
+//   rotation += 90; // add 90 degrees, you can change this as you want
+//       if (rotation === 360) { 
+//         // 360 means rotate back to 0
+//         rotation = 0;
+//   return rotation
+// }
+
+// }
 
