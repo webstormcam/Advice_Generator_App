b4bcf7fe49337d7aa0185b5a077da6be8de77618
Cameron Chandler <webstormcam@gmail.com>
2022-04-02 07:20:46 -0400

    Project complete

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ let url = 'https://api.adviceslip.com/advice';
 
 
 
-fetch(url)
-.then(res => res.json())
-.then((out) => {
-  advice_num.innerHTML = `${out.slip.id}`
-  advice.innerHTML =`${out.slip.advice}`
-})
-.catch(err => { throw err });
+// fetch(url)
+// .then(res => res.json())
+// .then((out) => {
+//   advice_num.innerHTML = `${out.slip.id}`
+//   advice.innerHTML =`${out.slip.advice}`
+// })
+// .catch(err => { throw err });
 
 
 
